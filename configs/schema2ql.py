d6cb63f60e6feb087a02c3488ca3db34411deeaf
Valentin Kuznetsov <vkuznet@gmail.com>
2024-05-20 09:23:49 -0400

    update ql keys

--- configs/schema2ql.py
+++ configs/schema2ql.py
@@ -8,5 +8,6 @@ docs = json.load(open(ifile))
 for i in docs:
     desc = i.get('description', '').replace('"', '')
     units = i.get('units', '')
-    print('{"service": "MetaData", "key": "%s", "description": "%s", "units": "%s", "schema": "%s"},' \
-            % (i["key"], desc, units, schema))
+    dtype = i.get('type', 'string')
+    print('{"service": "MetaData", "key": "%s", "description": "%s", "units": "%s", "type": "%s", "schema": "%s"},' \
+            % (i["key"], desc, units, dtype, schema))
